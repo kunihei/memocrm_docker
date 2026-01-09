337fef98e39b2f8004612703ab28bf27c64774f9
kunihisa <>
2026-01-09 15:16:35 +0900

    重複コードの削除、RouteServiceは完了

--- memocrm-api/config/app.php
+++ memocrm-api/config/app.php
@@ -127,7 +127,6 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\RouteServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
