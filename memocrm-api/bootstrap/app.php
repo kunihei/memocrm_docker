838b8050ab63a124b90777b0f9a4c4cda8fc4d94
kunihisa <>
2026-05-08 17:21:39 +0900

    APIの時だけ401を返すように修正

--- memocrm-api/bootstrap/app.php
+++ memocrm-api/bootstrap/app.php
@@ -7,21 +7,23 @@ use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectGuestsTo(fn () => null);
         $middleware->alias([
             'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $e, $request) {
-            return response()->json([
-                'message_list' => ['認証が切れました']
-            ], 401);
+            if ($request->is('api/*') && $request->expectsJson()) {
+                return response()->json([
+                    'message_list' => ['認証が切れました']
+                ], 401);
+            }
+            return null;
         });
     })->create();
